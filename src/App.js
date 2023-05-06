20ae66f090e02eb9ec9e27414522dd53c1b1e003
DynamoAbir <99138761+DynamoAbir@users.noreply.github.com>
2023-05-06 22:47:18 +0600

    adding something new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div>
-      <button className="btn">Button</button>
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }
